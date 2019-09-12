57cff49c29747153244689fb4900914db98536b4
Bella Fernandez <bellafernandez@Bellas-MacBook-Air.local>
2019-09-12 01:38:01 -0700

    guess reset shuffle

--- my-app/src/components/Game/index.js
+++ my-app/src/components/Game/index.js
@@ -29,3 +29,28 @@ class Game extends Component {
       });
     };
   
+    handleIncorrectGuess = data => {
+        this.setState({
+          data: this.resetData(data),
+          score: 0
+        });
+      };
+    
+      resetData = data => {
+        const resetData = data.map(item => ({ ...item, clicked: false }));
+        return this.shuffleData(resetData);
+      };
+    
+      shuffleData = data => {
+        let i = data.length - 1;
+        while (i > 0) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = data[i];
+          data[i] = data[j];
+          data[j] = temp;
+          i--;
+        }
+        return data;
+      };
+    
+    
