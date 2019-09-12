e14fd6f5e8e67dcb7eb86abe24b46ab576027cb9
Bella Fernandez <bellafernandez@Bellas-MacBook-Air.local>
2019-09-12 01:37:12 -0700

    game code

--- my-app/src/components/Game/index.js
+++ my-app/src/components/Game/index.js
@@ -5,3 +5,27 @@ import Container from "../Container";
 import ClickItem from "../ClickItem";
 import Footer from "../Footer";
 import data from "../../data.json";
+
+class Game extends Component {
+    state = {
+      data,
+      score: 0,
+      topScore: 0
+    };
+  
+    componentDidMount() {
+      this.setState({ data: this.shuffleData(this.state.data) });
+    }
+  
+    handleCorrectGuess = newData => {
+      const { topScore, score } = this.state;
+      const newScore = score + 1;
+      const newTopScore = Math.max(newScore, topScore);
+  
+      this.setState({
+        data: this.shuffleData(newData),
+        score: newScore,
+        topScore: newTopScore
+      });
+    };
+  
