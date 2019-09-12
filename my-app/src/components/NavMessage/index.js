aaa444536bbe539111c153ea7d47f6ec65b4b23a
Bella Fernandez <bellafernandez@Bellas-MacBook-Air.local>
2019-09-12 02:21:43 -0700

    index.js complete

--- my-app/src/components/NavMessage/index.js
+++ my-app/src/components/NavMessage/index.js
@@ -24,3 +24,28 @@ class NavMessage extends Component {
       this.setState(newState);
     }
   }
+  renderMessage = () => {
+    switch (this.state.message) {
+    case "correct":
+      return "You guessed correctly!";
+    case "incorrect":
+      return "You guessed incorrectly!";
+    default:
+      return "Click an image to begin!";
+    }
+  };
+
+  render() {
+    return (
+      <li
+        className={this.state.animating ? this.state.message : ""}
+        onAnimationEnd={() => this.setState({ animating: false })}
+      >
+        {this.renderMessage()}
+      </li>
+    );
+  }
+}
+
+export default NavMessage;
+
