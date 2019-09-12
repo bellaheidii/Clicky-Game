892d1e5f7554786bb3dab0d08133f782ef48a947
Bella Fernandez <bellafernandez@Bellas-MacBook-Air.local>
2019-09-12 01:54:14 -0700

    NavM js code

---
+++ my-app/src/components/NavMessage/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./style.css";
+
+// NavMessage renders an li tag containing an message for the user
+
+class NavMessage extends Component {
+  state = {
+    message: "",
+    animating: false
+  };
+
+  componentDidUpdate({ score, topScore }, prevState) {
+    const newState = { animating: true };
+
+    if (score === 0 && topScore === 0) {
+      newState.message = "";
+    } else if (score === 0 && topScore > 0) {
+      newState.message = "incorrect";
+    } else {
+      newState.message = "correct";
+    }
+
+    if (score !== this.props.score || this.state.message !== newState.message) {
+      this.setState(newState);
+    }
+  }
